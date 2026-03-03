fb0cb776d90aebc7500551e4881e491ed6f307fa
Oleg Repalo <altwulf7@gmail.com>
2026-03-03 19:24:49 +0200

    Add imports to __init__ for systems

--- body_sim/systems/__init__.py
+++ body_sim/systems/__init__.py
@@ -1,3 +1,66 @@
-# === systems/__init__.py ===
+# body_sim/systems/__init__.py
 
-__all__ = []
+# Инфляция и растяжение тканей
+from body_sim.systems.inflation import (
+    StretchState,
+    InflationProfile,
+    InflationSystem
+)
+
+# Управление вставленными объектами
+from body_sim.systems.insertion import (
+    InsertionType,
+    InsertedObject,
+    InsertionManager
+)
+
+# Лактация
+from body_sim.systems.lactation import (
+    LactationState,
+    LactationProfile,
+    LactationSystem
+)
+
+# Физика
+from body_sim.systems.physics import (
+    PhysicsState,
+    PhysicsEngine
+)
+
+# Давление
+from body_sim.systems.pressure import (
+    PressureTier,
+    PressureController
+)
+
+# Статистика
+from body_sim.systems.stats import StatisticsService
+
+__all__ = [
+    # Inflation
+    'StretchState',
+    'InflationProfile',
+    'InflationSystem',
+    
+    # Insertion
+    'InsertionType',
+    'InsertedObject',
+    'InsertionManager',
+    
+    # Lactation
+    'LactationState',
+    'LactationProfile',
+    'LactationSystem',
+    
+    # Physics
+    'PhysicsState',
+    'PhysicsEngine',
+    
+    # Pressure
+    'PressureTier',
+    'PressureController',
+    
+    # Stats
+    'StatisticsService',
+]
+
